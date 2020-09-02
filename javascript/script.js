15adf3d8a5d7523e6fd74757d5bbc49be8358fb2
Shivani Jadon <shivanijadon98@gmail.com>
2020-09-03 00:32:51 +0530

    add active state to slider controllers when clicked

--- javascript/script.js
+++ javascript/script.js
@@ -13,4 +13,36 @@
     });
 
     
+    var customizedFunction = function (info, eventName) {
+        // direct access to info object
+        console.log(info.displayIndex);
+        let active_slide = info.displayIndex;
+
+        let active_btn = `<img class="slider-btn" src="assets/images/button-act.png" />`;
+        let inactive_btn = `<img class="slider-btn" src="assets/images/button.png" />`;
+        let ctr1 = document.getElementById('controller-1');
+        let ctr2 = document.getElementById('controller-2');
+        let ctr3 = document.getElementById('controller-3');
+
+        switch (active_slide) {
+            case 1: ctr1.innerHTML = active_btn;
+                ctr2.innerHTML = inactive_btn;
+                ctr3.innerHTML = inactive_btn;
+                break;
+
+            case 2: ctr1.innerHTML = inactive_btn;
+                ctr2.innerHTML = active_btn;
+                ctr3.innerHTML = inactive_btn;
+                break;
+
+            case 3: ctr1.innerHTML = inactive_btn;
+                ctr2.innerHTML = inactive_btn;
+                ctr3.innerHTML = active_btn;
+                break;
+        }
+    }
+
+    // bind function to event
+    slider.events.on('indexChanged', customizedFunction);
+
 }
