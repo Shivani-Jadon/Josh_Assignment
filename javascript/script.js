1ff233ea0296c259261d5ccbca9d4c473657998c
Shivani Jadon <shivanijadon98@gmail.com>
2020-09-02 13:55:52 +0530

    add script to the html

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+{
+    console.log("Script loaded");
+    //implementing functionality of slider
+
+    //object storing images for slides
+    let slide_images = {
+        slide1: {
+            src: '../assets/images/banner_slider_01.jpg'
+        },
+        slide2: {
+            src: '../assets/images/banner_slider_02.jpg'
+        },
+        slide3: {
+            src: '../assets/images/banner_slider_01.jpg'
+        }
+    }
+
+    
+}
