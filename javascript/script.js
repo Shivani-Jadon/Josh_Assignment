e5909b0db13769c4c32033df418c1a1ff0837c09
Shivani Jadon <shivanijadon98@gmail.com>
2020-09-02 22:07:24 +0530

    make functional slider using tiny-slider

--- javascript/script.js
+++ javascript/script.js
@@ -2,18 +2,15 @@
     console.log("Script loaded");
     //implementing functionality of slider
 
-    //object storing images for slides
-    let slide_images = {
-        slide1: {
-            src: '../assets/images/banner_slider_01.jpg'
-        },
-        slide2: {
-            src: '../assets/images/banner_slider_02.jpg'
-        },
-        slide3: {
-            src: '../assets/images/banner_slider_01.jpg'
-        }
-    }
+    let slider = tns({
+        mode: 'gallery',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        controls: false,
+        navPosition: 'bottom-right',
+        navContainer: '.slider-controller'
+    });
 
     
 }
