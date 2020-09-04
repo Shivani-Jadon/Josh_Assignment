92c71afc36d2baa3400008132d092ef76e2993b3
Shivani Jadon <shivanijadon98@gmail.com>
2020-09-05 00:50:15 +0530

    update minor changes throughout page

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@
     console.log("Script loaded");
     //implementing functionality of slider
 
-    let slider = tns({
+    var slider = tns({
         mode: 'gallery',
         items: 1,
         slideBy: 'page',
@@ -16,13 +16,13 @@
     var customizedFunction = function (info, eventName) {
         // direct access to info object
         console.log(info.displayIndex);
-        let active_slide = info.displayIndex;
+        var active_slide = info.displayIndex;
 
-        let active_btn = `<img class="slider-btn" src="assets/images/button-act.png" />`;
-        let inactive_btn = `<img class="slider-btn" src="assets/images/button.png" />`;
-        let ctr1 = document.getElementById('controller-1');
-        let ctr2 = document.getElementById('controller-2');
-        let ctr3 = document.getElementById('controller-3');
+        var active_btn = `<img class="slider-btn" src="assets/images/button-act.png" />`;
+        var inactive_btn = `<img class="slider-btn" src="assets/images/button.png" />`;
+        var ctr1 = document.getElementById('controller-1');
+        var ctr2 = document.getElementById('controller-2');
+        var ctr3 = document.getElementById('controller-3');
 
         switch (active_slide) {
             case 1: ctr1.innerHTML = active_btn;
@@ -51,31 +51,31 @@
     //  Implementing images pop ups through modals
 
     //list of images for op ups
-    let pop_images_src = [
+    var pop_images_src = [
         "assets/images/full-photo.png",
         "assets/images/lifestyle_thmb_img01.png",
         "assets/images/lifestyle_thmb_img02.png",
         "assets/images/lifestyle_thmb_img03.png"
     ];
-    let img_count = pop_images_src.length;
+    var img_count = pop_images_src.length;
 
-    let current_image = 0;
+    var current_image = 0;
 
     // Get the modal
-    let img_modal = document.getElementById("image-Modal");
-    let image_frame = document.getElementById("modal-image");
+    var img_modal = document.getElementById("image-Modal");
+    var image_frame = document.getElementById("modal-image");
 
     // Get the thumbnails that opens the image-modal
-    let image_thumbnails = document.getElementsByClassName("image-thumbnail");
+    var image_thumbnails = document.getElementsByClassName("image-thumbnail");
 
     // Get the <span> element that closes the modal
-    let close_btns_list = document.getElementsByClassName("close-image");
+    var close_btns_list = document.getElementsByClassName("close-image");
 
     // When the user clicks on the thumnail, open the modal
-    for (let img_thumbnail of image_thumbnails) {
+    for (var img_thumbnail of image_thumbnails) {
 
         img_thumbnail.onclick = function () {
-            let thumb_no = img_thumbnail.getAttribute('thumb');
+            var thumb_no = img_thumbnail.getAttribute('thumb');
             image_frame.src = pop_images_src[thumb_no];
             img_modal.style.display = "flex";
             current_image = thumb_no;
@@ -83,7 +83,7 @@
         }
     }    
 
-    let next_btn = document.getElementsByClassName('next-image')[0];
+    var next_btn = document.getElementsByClassName('next-image')[0];
     // When the user clicks on next-image btn
     next_btn.onclick = function (event) {
         if (current_image < img_count - 1) {
@@ -95,7 +95,7 @@
         }
     }
 
-    let prev_btn = document.getElementsByClassName('prev-image')[0];
+    var prev_btn = document.getElementsByClassName('prev-image')[0];
     // When the user clicks on prev-image btn
     prev_btn.onclick = function (event) {
         if (current_image > 0) {
@@ -107,7 +107,7 @@
         }
     }
     // When the user clicks on <span> (x), close the modal
-    for (let close_btn of close_btns_list) {
+    for (var close_btn of close_btns_list) {
         close_btn.onclick = function () {
             img_modal.style.display = "none";
         }
@@ -117,29 +117,29 @@
 
 
      //  Implementing videos pop ups through modals
-    let video_srcs = [
+    var video_srcs = [
         "https://www.youtube.com/embed/07d2dXHYb94",
         "https://www.youtube.com/embed/8AGgbIQyqR8",
         "https://www.youtube.com/embed/vPuRBiBCxyk",
         "https://www.youtube.com/embed/DtujJRFuIi0"
     ];
 
-    let video_count = video_srcs.length;
-    let current_video = 0;
+    var video_count = video_srcs.length;
+    var current_video = 0;
 
     // Get the modal
-    let video_modal = document.getElementById("video-Modal");
+    var video_modal = document.getElementById("video-Modal");
     // get the iframe element
-    let video_frame = document.getElementById("video-frame");
+    var video_frame = document.getElementById("video-frame");
 
     // Get the thumbnails that opens the image-modal
-    let video_thumbnails = document.getElementsByClassName("video-thumbnails");
+    var video_thumbnails = document.getElementsByClassName("video-thumbnails");
 
     // When the user clicks on the video thumnail, open the modal
-    for (let video_thumb of video_thumbnails) {
+    for (var video_thumb of video_thumbnails) {
 
         video_thumb.onclick = function () {
-            let thumb_no = video_thumb.getAttribute('thumb');
+            var thumb_no = video_thumb.getAttribute('thumb');
             video_frame.src = video_srcs[thumb_no];
             video_modal.style.display = "flex";
             current_video = thumb_no;
@@ -147,7 +147,7 @@
         }
     }    
 
-    let next_btn_vid = document.getElementsByClassName('next-video')[0];
+    var next_btn_vid = document.getElementsByClassName('next-video')[0];
     // When the user clicks on next-image btn
     next_btn_vid.onclick = function (event) {
         if (current_video < video_count - 1) {
@@ -159,7 +159,7 @@
         }
     }
 
-    let prev_btn_vid = document.getElementsByClassName('prev-video')[0];
+    var prev_btn_vid = document.getElementsByClassName('prev-video')[0];
     // When the user clicks on prev-image btn
     prev_btn_vid.onclick = function (event) {
         if (current_video > 0) {
@@ -171,9 +171,9 @@
         }
     }
 
-    let close_btns = document.getElementsByClassName('close-video');
+    var close_btns = document.getElementsByClassName('close-video');
     // When the user clicks on <span> (x), close the modal
-    for (let close_btn of close_btns) {
+    for (var close_btn of close_btns) {
         close_btn.onclick = function () {
             video_modal.style.display = "none";
         }
